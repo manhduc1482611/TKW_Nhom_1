ec0b13f24b0fb9e8c3018d01a923e42492823491
manhduc1482611 <manhduc1482611@gmail.com>
2026-05-22 17:50:34 +0700

    ok

--- src/js/layouts/call.js
+++ src/js/layouts/call.js
@@ -11,8 +11,15 @@ fetch('/src/components/footer.html')
     .then(res => res.text())
     .then(html => {
         document.getElementById('footer').innerHTML = html;
-        // Nạp kèm file script xử lý active menu & giỏ hàng của header
         const s = document.createElement('script');
-        s.src = '/src/js/layouts/header.js';
+        s.src = '/src/js/layouts/footer.js';
+        document.body.appendChild(s);
+    });
+fetch('/src/components/chatbot.html')
+    .then(res => res.text())
+    .then(html => {
+        document.getElementById('chatbot').innerHTML = html;
+        const s = document.createElement('script');
+        s.src = '/src/js/layouts/chatbot.js';
         document.body.appendChild(s);
     });
