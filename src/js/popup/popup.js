7a7a360a1acb393c786a1bb1a8e77465d11e9851
hyen <27a4043376@hvnh.edu.vn>
2026-06-07 09:51:10 +0700

    Update popup.js

--- src/js/popup/popup.js
+++ src/js/popup/popup.js
@@ -1,22 +1,26 @@
 /* =========================================
    POPUP
 ========================================= */
-setTimeout(() => {
-  document.getElementById("popupOverlay").classList.add("active");
-}, 1000);
+document.addEventListener("DOMContentLoaded", () => {
 
-document.getElementById("popupClose").addEventListener("click", (e) => {
-  e.stopPropagation();
-  document.getElementById("popupOverlay").classList.remove("active");
-});
+  setTimeout(() => {
+    document.getElementById("popupOverlay").classList.add("active");
+  }, 1000);
 
-document.getElementById("popupOverlay").addEventListener("click", (e) => {
-  if (e.target.id === "popupOverlay") {
+  document.getElementById("popupClose").addEventListener("click", (e) => {
+    e.stopPropagation();
     document.getElementById("popupOverlay").classList.remove("active");
-  }
-});
+  });
 
-document.querySelector(".popup-box a").addEventListener("click", (e) => {
-  e.stopPropagation();
-  window.location.href = "/pages/promotion/html/promotion.html";
+  document.getElementById("popupOverlay").addEventListener("click", (e) => {
+    if (e.target.id === "popupOverlay") {
+      document.getElementById("popupOverlay").classList.remove("active");
+    }
+  });
+
+  document.querySelector(".popup-box a").addEventListener("click", (e) => {
+    e.stopPropagation();
+    window.location.href = "/pages/promotion/html/promotion.html";
+  });
+
 });
